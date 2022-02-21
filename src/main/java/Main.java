d87568b8c5bb6ba3f46121268fde334417a640c2
Brigitta Arvai <arvaibrigitta@yahoo.co.uk>
2022-02-21 12:31:43 +0100

    add basic class files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import manager.DatabaseManager;
+
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        DatabaseManager dm = new DatabaseManager();
+        dm.connect();
 
     }
 }
